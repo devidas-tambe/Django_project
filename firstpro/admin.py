e750f597c093f2c7c665686d5cc465ab7389dddf
Devidas Tambe <devitambe102@gmail.com>
2026-08-04 16:59:24 +0530

    Update admin.py

--- firstpro/admin.py
+++ firstpro/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
-from .models import student, employee, register, doc1
+from .models import grandparent, student, employee, register, doc1, Author, Book, customer, seller, job, work
 
 admin.site.register([student])
 admin.site.register([employee])
 admin.site.register([register])
 admin.site.register([doc1])
+admin.site.register([Author])
+admin.site.register([Book])
+admin.site.register([customer])
+admin.site.register([seller])   
+  
+admin.site.register([grandparent])  
+admin.site.register([job])
+admin.site.register([work]) 
+
