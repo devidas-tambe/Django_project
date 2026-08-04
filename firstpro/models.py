b76a7e0e6aaff9b97f10e88c0afb2660e9cc001c
Devidas Tambe <devitambe102@gmail.com>
2026-08-04 16:59:47 +0530

    Add models for Author, Book, and user types

--- firstpro/models.py
+++ firstpro/models.py
@@ -23,3 +23,52 @@ class register(models.Model):
 class doc1(models.Model):
     fullname=models.CharField(max_length=100)
     photo=models.FileField(upload_to='images')
+
+class Author(models.Model):
+    name=models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title=models.CharField(max_length=100)
+    at=models.ForeignKey(Author,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+
+class admin1(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=100)
+
+    class Meta:
+        abstract=True
+
+class customer(admin1):
+    c_id=models.CharField(max_length=100)
+
+class seller(admin1):
+    s_id=models.CharField(max_length=100)
+
+class child1(models.Model):
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+
+    class Meta:
+        abstract=True
+
+class parent(child1):
+    profile=models.CharField(max_length=100)
+
+class grandparent(child1):
+    identity=models.CharField(max_length=100)
+
+class job(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=100)
+    task=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class work(job):
+    class Meta:
+        proxy=True
